c00765379470901d74dd8096f08381e13fd322ce
Roman <51695968+RProcop@users.noreply.github.com>
2026-02-18 20:26:42 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
+function openContact() {
+  document.getElementById("contactModal").style.display = "flex";
+}
+
+function closeContact() {
+  document.getElementById("contactModal").style.display = "none";
+}
 
