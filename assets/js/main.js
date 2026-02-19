845a1fe2892ccc9b2c1f8f1b92b5eaa5e72be520
Roman <51695968+RProcop@users.noreply.github.com>
2026-02-19 11:02:06 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,8 @@
-const modal = document.getElementById("contactModal");
 
 function openContact() {
-  modal.classList.add("open");
-  document.body.style.overflow = "hidden"; // блокируем скролл
+  document.getElementById("contactModal").style.display = "flex";
 }
 
 function closeContact() {
-  modal.classList.remove("open");
-  document.body.style.overflow = ""; // возвращаем скролл
+  document.getElementById("contactModal").style.display = "none";
 }
-
-/* Закрытие по клику вне модального окна */
-modal.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    closeContact();
-  }
-});
-
-/* Закрытие по ESC */
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && modal.classList.contains("open")) {
-    closeContact();
-  }
-});
