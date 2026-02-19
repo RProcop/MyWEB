71f72ea9f5756c5bee895aa08f0f4a0f4df8b76e
Roman <51695968+RProcop@users.noreply.github.com>
2026-02-19 21:41:19 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 function openContact() {
   document.getElementById("contactModal").style.display = "flex";
 }
@@ -6,54 +5,3 @@ function openContact() {
 function closeContact() {
   document.getElementById("contactModal").style.display = "none";
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-
-  /* ================= LANGUAGE SWITCH ================= */
-
-  const langSwitch = document.getElementById("langSwitch");
-  const langCurrent = document.getElementById("langCurrent");
-  const currentLangLabel = document.getElementById("currentLang");
-  const langItems = document.querySelectorAll(".lang-item");
-
-  if (!langSwitch || !langCurrent) return; // защита от ошибок
-
-  langCurrent.addEventListener("click", () => {
-    langSwitch.classList.toggle("open");
-  });
-
-  document.addEventListener("click", (e) => {
-    if (!e.target.closest(".lang-switch")) {
-      langSwitch.classList.remove("open");
-    }
-  });
-
-  langItems.forEach(item => {
-    item.addEventListener("click", () => {
-      const lang = item.dataset.lang;
-
-      if (lang === "ua") {
-        document.querySelector(".hero-left h1").innerHTML =
-          "ВБУДОВАНІ<br>РІШЕННЯ<br>НДДКР<br>3D Друк<br>Моделювання";
-
-        document.querySelector(".subtitle").innerHTML =
-          "Інженерні IoT системи.<br>Надійні апаратні & програмні рішення.<br>3D Друк та моделювання.";
-
-        currentLangLabel.textContent = "UA";
-      }
-
-      if (lang === "en") {
-        document.querySelector(".hero-left h1").innerHTML =
-          "EMBEDDED<br>IOT<br>R&D<br>3D Printing<br>Modeling";
-
-        document.querySelector(".subtitle").innerHTML =
-          "Engineering-driven IoT systems.<br>Reliable hardware & firmware solutions.<br>3D Printing & Modeling.";
-
-        currentLangLabel.textContent = "EN";
-      }
-
-      langSwitch.classList.remove("open");
-    });
-  });
-
-});
